73cbe09b625078baee2d4bc70840095528377753
AitZak <zackari.aitelhadi@gmail.com>
2019-08-22 14:36:48 +0200

    first commit

---
+++ src/Controller/UserController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+
+class UserController extends FOSRestController
+{
+    private $userRepository;
+    private $em;
+
+    public function __construct(UserRepository $userRepository, EntityManagerInterface $em)
+    {
+        $this->userRepository = $userRepository;
+        $this->em = $em;
+    }
+    /**
+     * @Rest\Get("/api/users/{email}")
+     */
+    public function getApiUser(User$user)
+    {
+        return $this->view($user);
+    }
+
+    /**    *
+     * @Rest\Get("/api/users")
+     */
+    public function getApiUsers()
+    {
+        $users = $this->userRepository->findAll();
+        return $this->view($users);
+    }
+
+    /**
+     * @Rest\Post("/api/users")
+     * @ParamConverter("user", converter="fos_rest.request_body")
+     */
+    public function postApiUser(User $user, ValidatorInterface $validator )
+    {
+        $validationErrors = $validator->validate($user);
+        if ($validationErrors->count() > 0) {
+            /** @var ConstraintViolation $constraintViolation */
+            foreach ($validationErrors as $constraintViolation) {
+                // Returns the violation message. (Ex. This value should not be blank.)
+                $message = $constraintViolation->getMessage();
+                // Returns the property path from the root element to the violation. (Ex. lastname)
+                $propertyPath = $constraintViolation->getPropertyPath();
+                $errors[] =['message' => $message, 'propertyPath' => $propertyPath];
+            }
+}
+if (!empty($errors)) {
+    // Throw a 400 Bad Request with all errors messages (Not readable, you can do better)
+    throw new BadRequestHttpException(\json_encode($errors));
+}
+        $this->em->persist($user);
+        $this->em->flush();
+        return $this->view($user);
+    }
+
+    /**
+     * @Rest\Patch("/api/users/{email}")
+     * @Rest\View(serializerGroups={"user"})
+     */
+    public function patchApiUser(User $user, Request $request, ValidatorInterface $validator)
+    {
+        $validationErrors = $validator->validate($user);
+        if ($validationErrors->count() > 0) {
+            /** @var ConstraintViolation $constraintViolation */
+            foreach ($validationErrors as $constraintViolation) {
+                // Returns the violation message. (Ex. This value should not be blank.)
+                $message = $constraintViolation->getMessage();
+                // Returns the property path from the root element to the violation. (Ex. lastname)
+                $propertyPath = $constraintViolation->getPropertyPath();
+                $errors[] =['message' => $message, 'propertyPath' => $propertyPath];
+            }
+        }
+        if (!empty($errors)) {
+            // Throw a 400 Bad Request with all errors messages (Not readable, you can do better)
+            throw new BadRequestHttpException(\json_encode($errors));
+        }
+
+        $attributes = [
+          'firstname' => 'setFirstname',
+          'lastname' => 'setLastname',
+        ];
+
+        foreach ( $attributes as $attribute => $setter){
+            if ($request->get($attribute) === null){
+                continue;
+            }
+            $user->$setter($request->get($attribute));
+        }
+        $this->em->flush();
+        return $this->view($user);
+    }
+
+    /**
+     * @Rest\Delete("/api/users/{email}")
+     */
+    public function deleteApiUser(User $user)
+    {
+        $this->em->remove($user);
+        $this->em->flush();
+        return new Response(null, 204);
+    }
+
+    /**    *
+     * @Rest\Get("/api/admin/users")
+     */
+    public function getApiAdminUsers()
+    {
+        $users = $this->userRepository->findAll();
+        return $this->view($users);
+    }
+}
---
+++ tests/UsersControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UsersControllerTest extends WebTestCase
+{
+    public function testGetUsers()
+    {
+        $client = static::createClient();
+        $client->request('GET','api/users', [], [],
+            [
+                'HTTP_ACCEPT' => 'application/json',
+                'HTTP_X-AUTH-TOKEN' => 'ONXI@V73.ZMI'
+            ]);
+
+        $response = $client->getResponse();
+        $content = $response->getContent();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertJson($content);
+
+        $arrayContent = \json_decode($content, true);
+        $this->assertCount(10, $arrayContent);
+    }
+
+    public function testPostUsers()
+    {
+        $client = static::createClient();
+        $client->request('POST', '/api/users', [], [],[
+            'HTTP_ACCEPT' => 'application/json',
+            'CONTENT_TYPE' => 'application/json',
+            'HTTP_X-AUTH-TOKEN' => 'ONXI@V73.ZMI'
+            ],
+            '{"apiKey": "adqaaa","email": "test@behat.com"}'
+        );
+
+        $response = $client->getResponse();
+        $content = $response->getContent();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertJson($content);
+    }
+
+}
